a9b8a9d0cafacfe48bbf39bee51ee132e73208db
LidiiaChyrnyshuk <l.mandych@gmail.com>
2025-07-24 22:37:34 +0200

    add html.css.js

---
+++ js/language.js
@@ -0,0 +1,242 @@
+
+  const translations = {
+		EN: {
+			title: "welcome",
+			titleBonus: "BONUS",
+			promoSubtext: "up to",
+			promoCurrency: "3000 USDT",
+			modalPromoCurrency: "3000 USDT",
+			bonus: "+ 100 Free spins",
+			modalBonus: "+ 100 Free spins",
+			deposit: "without deposit",
+			btnRegistration: "JOIN NOW",
+			titleBenefits: "Best for Casino Guru players",
+			benefitsTop: "TOP 10",
+			benefitsBest: "SAFETY INDEX",
+			benefitsYear: "HIGH",
+			casinoRank: "Exclusive bonuses",
+			great: "Great",
+			playerRating: "Sharing 3% from income casino profit",
+			reviews: "817 reviews",
+			gamesTitel: "Over 8000+ games",
+			btnPlay: "Play",
+			checkbox:
+				"By ticking this box to register for this website, the user declares to be over 18 years old and to have read, understood and accepted",
+			terms: "the Terms and Conditions",
+			modalBtn: "Create an account",
+			quantityCharacter: "Contain at least 6 characters",
+			uppercaseCharacter: "Include at least 1 uppercase letter",
+			numberCharacter: "Include at least 1 number",
+			emailError: "Incorrect email format",
+		},
+		HU: {
+			title: "Üdvözlő bónusz",
+			promoSubtext: "up to",
+			modalPromoSubtext: "up to",
+			percent: "350%",
+			modalPercent: "350%",
+			promoCurrency: "2 800 000 HUF",
+			modalPromoCurrency: "2 800 000 HUF",
+			bonus: "+ 50 ingyenes pörgetés",
+			modalBonus: "+ 50 ingyenes pörgetés",
+			deposit: "befizetés nélkül",
+			btnRegistration: "Regisztráció",
+			titleBenefits: "A legjobb Casino Guru játékosoknak",
+			benefitsTop: "A 10",
+			benefitsBest: "Biztonsági index",
+			benefitsYear: " MAGAS",
+			casinoRank: "Exkluzív bónuszok",
+			great: "Nagyszerű",
+			playerRating: "A kaszinó profitjának 3%-át kapod meg",
+			reviews: "817 értékelés",
+			gamesTitel: "Több mint 8000 játék",
+			btnPlay: "Játssz!",
+			checkbox:
+				"A jelölőnégyzet kipipálásával a felhasználó kijelenti, hogy elmúlt 18 éves, és elolvasta, megértette és elfogadta",
+			terms: "a Felhasználási feltételeket",
+			modalBtn: "Fiók létrehozása",
+			quantityCharacter: "En az 6 karakter",
+			uppercaseCharacter: "En az 1 büyük harf",
+			numberCharacter: "En az 1 rakam",
+			emailError: "Hibás email formátum",
+		},
+		PT: {
+			title: "Bónus de boas-vindas",
+			promoSubtext: "up to",
+			promoCurrency: "6 800 EUR",
+			modalPromoCurrency: "6 800 EUR",
+			bonus: "+ 50 rodadas grátis",
+			modalBonus: "+ 50 rodadas grátis",
+			deposit: "sem depósito",
+			btnRegistration: "Regista-te",
+			titleBenefits: "Melhor para jogadores do Casino Guru",
+			benefitsTop: "TOP 10",
+			benefitsBest: "Índice de segurança",
+			benefitsYear: "ALTO",
+			casinoRank: "Casino RankBónus exclusivos",
+			great: "Ótimo",
+			playerRating: "Partilhando 3% do lucro do casino",
+			reviews: "817 avaliações",
+			gamesTitel: "Mais de 8 000 jogos",
+			btnPlay: "Joga",
+			checkbox:
+				"Ao marcar esta caixa para se registar neste site, o utilizador declara ser maior de 18 anos e ter lido, compreendido e aceite",
+			terms: "os Termos e Condições",
+			modalBtn: "Criar uma conta",
+			quantityCharacter: "Conter pelo menos 6 caracteres",
+			uppercaseCharacter: "Incluir pelo menos 1 letra maiúscula",
+			numberCharacter: "Incluir pelo menos 1 número",
+			emailError: "Formato de email incorreto",
+		},
+		RU: {
+			title: "welcome BONUS",
+			promoSubtext: "up to",
+			promoCurrency: "700 000 RUB",
+			modalPromoCurrency: "700 000 RUB",
+			bonus: "+ 50 фриспинов",
+			modalBonus: "+ 50 фриспинов",
+			deposit: "без депозита",
+			btnRegistration: "Регистрация",
+			titleBenefits: "Лучшее для игроков Casino Guru",
+			benefitsTop: "ТОП 10",
+			benefitsBest: "Индекс безопасности",
+			benefitsYear: "ВЫСОКИЙ",
+			casinoRank: "Эксклюзивные бонусы",
+			great: "Отлично",
+			playerRating: "Делимся 3% от прибыли казино",
+			reviews: "817 отзывов",
+			gamesTitel: "Более 8000+ игр",
+			btnPlay: "Играть",
+			checkbox:
+				"Ставя галочку в этом поле для регистрации на сайте, пользователь подтверждает, что ему больше 18 лет, и что он прочитал, понял и принял",
+			terms: "Условия и положения",
+			modalBtn: "Cоздать акаунт",
+			quantityCharacter: "Не менее 6 символов",
+			uppercaseCharacter: "Как минимум 1 заглавную букву",
+			numberCharacter: "Как минимум 1 цифру",
+			emailError: "Неверный формат email",
+		},
+		CZ: {
+			title: "Uvítací bonus",
+			promoSubtext: "up to",
+			promoCurrency: "6 800 EUR",
+			modalPromoCurrency: "6 800 EUR",
+			bonus: "+ 50 roztočení zdarma",
+			modalBonus: "+ 50 roztočení zdarma",
+			deposit: "bez vkladu",
+			btnRegistration: "Registrovat",
+			titleBenefits: "Nejlepší pro hráče Casino Guru",
+			benefitsTop: "TOP 10",
+			benefitsBest: "Index bezpečnosti",
+			benefitsYear: "VYSOKÝ",
+			casinoRank: "Exkluzivní bonusy",
+			great: "Skvělé",
+			playerRating: "Sdílení 3 % z příjmu, zisk kasina",
+			reviews: "817 recenzí",
+			gamesTitel: "Více než 8 000 her",
+			btnPlay: "Hrát",
+			checkbox:
+				"Zaškrtnutím tohoto políčka pro registraci na webu uživatel potvrzuje, že je mu více než 18 let, a že si přečetl, pochopil a přijal",
+			terms: "Podmínky použití",
+			modalBtn: "Vytvořit účet",
+			quantityCharacter: "Mít alespoň 6 znaků",
+			uppercaseCharacter: "Obsahovat alespoň 1 velké písmeno",
+			numberCharacter: "Obsahovat alespoň 1 číslici",
+			emailError: "Nesprávný formát e-mailu",
+		},
+		CA: {
+			title: "welcome BONUS",
+			promoSubtext: "up to",
+			promoCurrency: "10 000 CAD",
+			modalPromoCurrency: "10 000 CAD",
+			bonus: "+50 Free spins",
+			modalBonus: "+ 50 Free spins",
+			deposit: "without deposit",
+			btnRegistration: "Registration",
+			titleBenefits: "Best for Casino Guru players",
+			benefitsTop: "TOP 10",
+			benefitsBest: "Safety index",
+			benefitsYear: "HIGH",
+			casinoRank: "Exclusive bonuses",
+			great: "Great",
+			playerRating: "Sharing 3% from income casino profit",
+			reviews: "817 reviews",
+			gamesTitel: "Over 8000+ games",
+			btnPlay: "Play",
+			checkbox:
+				"By ticking this box to register for this website, the user declares to be over 18 years old and to have read, understood and accepted",
+			terms: "the Terms and Conditions",
+			modalBtn: "Create an account",
+			quantityCharacter: "Contain at least 6 characters",
+			uppercaseCharacter: "Include at least 1 uppercase letter",
+			numberCharacter: "Include at least 1 number",
+			emailError: "Incorrect email format",
+		},
+	};
+
+  const btn = document.getElementById("lang-btn");
+	const dropdown = document.getElementById("lang-dropdown");
+	const currentLang = document.getElementById("current-lang");
+
+	const fallbackLang = "EN";
+
+	const applyLanguage = (lang) => {
+		if (!translations[lang]) return;
+
+		document.body.className = `lang-${lang}`;
+
+		document.querySelectorAll("[data-translate]").forEach((el) => {
+			const key = el.dataset.translate;
+			const fallbackLang = "EN";
+			const translation =
+				translations[lang][key] ?? translations[fallbackLang][key];
+
+			if (translation !== undefined) {
+				el.textContent = translation;
+			}
+		});
+
+		currentLang.textContent = lang;
+		localStorage.setItem("selectedLang", lang);
+
+		// Активна мова — підсвітка
+		document.querySelectorAll("#lang-dropdown li").forEach((li) => {
+			if (li.dataset.lang === lang) {
+				li.classList.add("active");
+			} else {
+				li.classList.remove("active");
+			}
+		});
+	};
+
+	// Відкриття / закриття списку мов
+	btn.addEventListener("click", () => {
+		dropdown.classList.toggle("hidden");
+		btn.classList.toggle("open");
+	});
+
+	// Обробка кліку по мові
+	dropdown.querySelectorAll("li").forEach((item) => {
+		item.addEventListener("click", () => {
+			const lang = item.dataset.lang;
+			applyLanguage(lang);
+			dropdown.classList.add("hidden");
+			btn.classList.remove("open");
+		});
+	});
+
+	// Закриття випадаючого списку при кліку поза ним
+	document.addEventListener("click", (e) => {
+		if (!btn.contains(e.target) && !dropdown.contains(e.target)) {
+			dropdown.classList.add("hidden");
+			btn.classList.remove("open");
+		}
+	});
+
+	// Завантаження мови при відкритті сторінки
+	window.addEventListener("DOMContentLoaded", () => {
+		const savedLang = localStorage.getItem("selectedLang") || "EN";
+		applyLanguage(savedLang);
+	});
+
+	
